cb4734d64c3ddaab5086ca063e90be6687d82ca6
titigmr <56739959+titigmr@users.noreply.github.com>
2019-11-23 19:02:50 +0100

    Add files via upload

---
+++ git_scala/scala/Parser.scala
@@ -0,0 +1,20 @@
+import scala.collection.mutable.ListBuffer
+import scala.io.Source
+
+class Parser {
+
+  val document = Source.fromFile("consignes_2.txt").getLines.toList
+
+  var coord_xmax = document(0).split(" ")(0).toInt
+  var coord_ymax = document(0).split(" ")(0).toInt
+
+    var commandes = new ListBuffer[String]()
+    var position = new ListBuffer[String]()
+
+    for ((line, count) <- document.zipWithIndex) {
+      if (count % 2 == 0 && count != 0) commandes += line
+      if (count % 2 != 0 && count != 0) position += line
+    }
+}
+
+
